bec34d5b74253041b2ef2163e9b6c7c8cc677bcb
ladlod <tian_zw@qq.com>
2019-11-26 16:29:30 +0800

    add75

---
+++ leetcode75.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class leetcode75 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] matrix = new int[n];
+        for (int i = 0; i < n; i++) {
+            matrix[i] = in.nextInt();
+        }
+        sortColors(matrix);
+        for(int i = 0; i < n; i++)
+            System.out.print(matrix[i] + " ");
+        in.close();
+    }
+
+    static void sortColors(int[] nums) {
+        int i = 0, begin = 0, end = nums.length - 1;
+        while (i <= end) {
+            if(nums[i] == 0){
+                int tmp = nums[i];
+                nums[i] = nums[begin];
+                nums[begin] = tmp;
+                begin++;
+                i++;
+            }
+            else if(nums[i] == 2){
+                int tmp = nums[i];
+                nums[i] = nums[end];
+                nums[end] = tmp;
+                end--;
+            }
+            else
+                i++;
+        }
+    }
+}
