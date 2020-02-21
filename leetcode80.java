da3f84d07f5fecdf8555e04c1ada52c1e1d8174b
ladlod <tian_zw@qq.com>
2020-02-22 00:31:11 +0800

    add 80

---
+++ leetcode80.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class leetcode80{
+    public static void main(String[] args){ 
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = in.nextInt();
+        }
+        System.out.println(removeDuplicates(nums));
+        for(int i = 0; i < n; i++){
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+        in.close();
+    }
+
+    static int removeDuplicates(int[] nums) {
+        int x = 2;
+        for(int y = 2; y < nums.length; y++){
+            nums[x] = nums[y];
+            if(!(nums[x] == nums[x - 1] && nums[x] == nums[x - 2])){
+                x++;
+            }
+        }
+
+        return x;
+    }
+}
