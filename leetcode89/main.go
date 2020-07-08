dfa2415b58275c50f982bbcf440240a5668e4800
ladlod <tian_zw@qq.com>
2020-07-08 16:45:49 +0800

    add 89

---
+++ leetcode89/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
+	fmt.Println(grayCode(n))
+}
+func grayCode(n int) []int {
+	rtn := make([]int, 0)
+	rtn = append(rtn, 0)
+
+	nums := make([]int, n)
+
+	_map := make(map[int]bool)
+	_map[0] = true
+
+tag:
+	for true {
+		for i := 0; i < n; i++ {
+			if nums[i] == 0 {
+				nums[i] = 1
+			} else {
+				nums[i] = 0
+			}
+			num := arrayToNum(nums)
+			if _, ok := _map[num]; !ok {
+				rtn = append(rtn, num)
+				_map[num] = true
+				continue tag
+			}
+			if nums[i] == 0 {
+				nums[i] = 1
+			} else {
+				nums[i] = 0
+			}
+		}
+		break
+	}
+
+	return rtn
+}
+
+func arrayToNum(num []int) (rtn int) {
+	for i := 0; i < len(num); i++ {
+		rtn = rtn*2 + num[i]
+	}
+	return
+}
