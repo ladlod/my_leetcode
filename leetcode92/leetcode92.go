4ae455a2853bf2a531dcfcf069a96acff4f9885c
ladlod <tian_zw@qq.com>
2020-09-11 21:03:14 +0800

    add 92

---
+++ leetcode92/leetcode92.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	var m, n int
+	fmt.Scanf("%s", m)
+	fmt.Scanf("%s", n)
+	var l *ListNode
+	p := l
+	for i := 1; i <= 10; i++ {
+		p.Val = i
+		p = p.Next
+	}
+	reverseBetween(l, m, n)
+	for l != nil {
+		fmt.Printf("%d ", l.Val)
+		l = l.Next
+	}
+}
+
+func reverseBetween(head *ListNode, m int, n int) *ListNode {
+	if m == n {
+		return head
+	}
+	p := head
+	for i := 1; i < n; i++ {
+		if i >= m-1 && i <= n {
+			for p.Next != nil && p.Next.Next != nil {
+				tmp1 := p.Next
+				tmp2 := p.Next.Next
+				p.Next = tmp2
+				tmp1.Next = tmp2.Next
+				tmp2.Next = tmp1
+				p = tmp1
+				i++
+			}
+		}
+		p = p.Next
+	}
+	return head
+}
