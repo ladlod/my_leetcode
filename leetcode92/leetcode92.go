8e84bf5a101b39ba2a22307f09b1830f86f1baef
ladlod <tian_zw@qq.com>
2020-09-14 11:37:22 +0800

    add 92

--- leetcode92/leetcode92.go
+++ leetcode92/leetcode92.go
@@ -9,15 +9,18 @@ type ListNode struct {
 
 func main() {
 	var m, n int
-	fmt.Scanf("%s", m)
-	fmt.Scanf("%s", n)
+	fmt.Scanf("%d", &m)
+	fmt.Scanf("%d", &n)
 	l := &ListNode{}
 	p := l
 	for i := 1; i <= 10; i++ {
 		p.Val = i
+		if i != 10 {
+			p.Next = new(ListNode)
+		}
 		p = p.Next
 	}
-	reverseBetween(l, m, n)
+	l = reverseBetween(l, m, n)
 	for l != nil {
 		fmt.Printf("%d ", l.Val)
 		l = l.Next
@@ -25,23 +28,39 @@ func main() {
 }
 
 func reverseBetween(head *ListNode, m int, n int) *ListNode {
-	if m == n {
+	if m >= n || m < 1 {
 		return head
 	}
-	p := head
-	for i := 1; i < n; i++ {
-		if i >= m-1 && i <= n {
-			for p.Next != nil && p.Next.Next != nil {
-				tmp1 := p.Next
-				tmp2 := p.Next.Next
-				p.Next = tmp2
-				tmp1.Next = tmp2.Next
-				tmp2.Next = tmp1
-				p = tmp1
-				i++
-			}
+	p := &ListNode{Val: 0}
+	rtn := p
+	p.Next = head
+	pnext, pre, start, end := &ListNode{}, &ListNode{}, &ListNode{}, &ListNode{}
+	for i := 0; i <= n+1; i++ {
+		if i == m-1 {
+			pre = p    //0
+			p = p.Next // 1
+			pre.Next = nil
+		} else if i == m {
+			start = p           //1
+			tmp := p.Next       //2
+			pnext = p.Next.Next //3
+			p.Next.Next = p     //2->1
+			p = tmp             //2
+			start.Next = nil
+		} else if i > m && i < n {
+			tmp := pnext.Next //4
+			pnext.Next = p    //3->2
+			p = pnext         //3
+			pnext = tmp       //4
+		} else if i == n {
+			end = p   // 3
+			p = pnext // 4
+		} else if i == n+1 {
+			pre.Next = end //0->3
+			start.Next = p //1->4
+		} else {
+			p = p.Next
 		}
-		p = p.Next
 	}
-	return head
+	return rtn.Next
 }
