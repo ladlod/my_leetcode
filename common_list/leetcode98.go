cf9b34f0db3b3bbabf6eb689d6185a0452db6a1a
ladlod <tian_zw@qq.com>
2021-07-16 18:02:02 +0800

    add 98

--- common_list/leetcode98.go
+++ common_list/leetcode98.go
@@ -1,14 +1,22 @@
 package common_list
 
-import "fmt"
-
 func isValidBST(root *TreeNode) bool {
-	if root == nil {
-		return true
-	} else if (root.Left != nil && root.Left.Val >= root.Val) ||
-		(root.Right != nil && root.Right.Val <= root.Val) {
-		fmt.Println(root.Left.Val, root.Right.Val)
-		return false
+	sortList := proTraverl(root)
+	for i := 0; i < len(sortList)-1; i++ {
+		if sortList[i] >= sortList[i+1] {
+			return false
+		}
 	}
-	return isValidBST(root.Right) && isValidBST(root.Left)
+	return true
+}
+
+func proTraverl(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+	res = append(res, proTraverl(root.Left)...)
+	res = append(res, root.Val)
+	res = append(res, proTraverl(root.Right)...)
+	return res
 }
