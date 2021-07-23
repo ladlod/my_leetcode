0ae303b712bd659c19132e0c7c20e6f4ae59a06c
ladlod <tian_zw@qq.com>
2021-07-23 19:43:20 +0800

    add 1292

--- common_list/leetcode1292.py
+++ common_list/leetcode1292.py
@@ -9,28 +9,35 @@
 # 链接：https://leetcode-cn.com/problems/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-# [1,2,3,4,5]
-# [1,2,3,4,5]
-# [1,2,3,4,5]
-
-# [1, 3, 6, 10, 15]
-# [2, 6, 12, 20, 30]
-# [3, 9, 18, 30, 45]
+# [1,1,3,2,4,3,2]
+# [1,1,3,2,4,3,2]
+# [1,1,3,2,4,3,2]
 
 def maxSideLength(mat, threshold):
     m = len(mat)
     if m <= 0 : return 0
     n = len(mat[0])
-    answer = [[0 for i in range(0, n)] for i in range(0, m)]
+    square = [[0 for i in range(0, n)] for i in range(0, m)]
     for i in range(0, m):
         for j in range(0, n):
-            x = answer[i-1][j] if i > 0 else 0
-            y = answer[i][j-1] if j > 0 else 0
-            z = answer[i-1][j-1] if i > 0 and j > 0 else 0
-            answer[i][j] = x + y - z + mat[i][j]
+            x = square[i-1][j] if i > 0 else 0
+            y = square[i][j-1] if j > 0 else 0
+            z = square[i-1][j-1] if i > 0 and j > 0 else 0
+            square[i][j] = x + y - z + mat[i][j]
 
+    res = -1
+    for l in range(1,min(m,n)):
+        for i in range(0,m-l):
+            for j in range(0,n-l):
+                x = square[i-1][j+l] if i > 0 else 0
+                y = square[i+l][j-1] if j > 0 else 0
+                z = square[i-1][j-1] if i > 0 and j > 0 else 0
+
+                if square[i+l][j+l] - x - y + z <= threshold:
+                    res = max(res, l)
+                    continue
     
-    return answer
+    return res + 1
 
 if __name__ == '__main__':
-    print(maxSideLength([[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]],0))
+    print(maxSideLength([[1,1,1,1],[1,0,0,0],[1,0,0,0],[1,0,0,0]],6))
