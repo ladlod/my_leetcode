3bd586a1df1e6d8b9b4253ac9fe48c6c5c7723ee
ladlod <tian_zw@qq.com>
2021-09-05 17:57:13 +0800

    add 128

--- common_list/leetcode128.go
+++ common_list/leetcode128.go
@@ -1,15 +1,52 @@
 package common_list
 
+import "fmt"
+
 // 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
 
 // 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+
+// eg:：
+
+// 输入：nums = [100,4,200,1,3,2]
+// 输出：4
+// 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
 
 // 来源：力扣（LeetCode）
 // 链接：https://leetcode-cn.com/problems/longest-consecutive-sequence
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 // 思路：
-//
+// 构造一个压缩列表
 func LongestConsecutive(nums []int) int {
-	return 0
+	numM := make(map[int]bool)
+
+	// 构造map
+	for _, num := range nums {
+		numM[num] = true
+	}
+
+	res := make(map[int]int)
+	for _, num := range nums {
+		if res[num] > 0 {
+			continue
+		}
+		for k := 0; numM[num-k]; k++ {
+			res[num-k] = k + 1
+		}
+	}
+
+	// [1 2 0 1]
+	// {}
+
+	fmt.Println(res)
+
+	r := 0
+	for _, v := range res {
+		if v > r {
+			r = v
+		}
+	}
+
+	return r
 }
--- main.go
+++ main.go
@@ -3,17 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/ladlod/leetcode/week_contest"
+	"github.com/ladlod/leetcode/common_list"
 )
 
 func main() {
-	test1, test2 := []int{}, []int{}
-	for i := 10000; i > 0; i-- {
-		test1 = append(test1, i)
-	}
-	for i := 1; i <= 10000; i++ {
-		test2 = append(test2, i)
-	}
-	fmt.Println(week_contest.GcdSort(test1))
-	fmt.Println(week_contest.GcdSort(test2))
+	fmt.Println(common_list.LongestConsecutive([]int{1, 2, 0, 1}))
 }
