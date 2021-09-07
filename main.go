b6943fc431a76d44bc8405d83be249dbb667e9f9
ladlod <tian_zw@qq.com>
2021-09-07 15:11:22 +0800

    rm

--- main.go
+++ main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/ladlod/leetcode/common_list"
-)
-
 func main() {
-	fmt.Println(common_list.NumIslands([][]byte{
-		{'1', '1', '1', '1', '0'},
-		{'1', '1', '0', '1', '0'},
-		// {"1", "1", "0", "0", "0"},
-		// {"0", "0", "0", "0", "0"},
-	}))
 }
