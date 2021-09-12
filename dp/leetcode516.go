2470e90f7ad6476c17f8d4cd0440434accb5454a
ladlod <tian_zw@qq.com>
2021-09-12 14:02:03 +0800

    add comment

--- dp/leetcode516.go
+++ dp/leetcode516.go
@@ -1,5 +1,19 @@
 package dp
 
+// 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+
+// 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/longest-palindromic-subsequence
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+// 思路:
+// 动态规划
+// dp[i][j]记录从i到j的最长回文字符串
+// dp[i][j] = if i == j then 1
+//            elif s[i] == s[j] then dp[i+1][j-1]+2
+//			  else max(dp[i-1][j], dp[i][j-1])
 func LongestPalindromeSubseq(s string) int {
 	l := len(s)
 	if l <= 0 {
--- week_contest/leetcode5870.go
+++ week_contest/leetcode5870.go
@@ -8,11 +8,24 @@ package week_contest
 
 // 节点 x 为根的 子树 包含节点 x 和它所有的 后代 节点。
 
+// 输入：parents = [-1,0,1,0,3,3], nums = [5,4,6,2,1,3]
+// 输出：[7,1,1,4,2,1]
+// 解释：每个子树答案计算结果如下：
+// - 0：子树内包含节点 [0,1,2,3,4,5] ，基因值分别为 [5,4,6,2,1,3] 。7 是缺失的最小基因值。
+// - 1：子树内包含节点 [1,2] ，基因值分别为 [4,6] 。 1 是缺失的最小基因值。
+// - 2：子树内只包含节点 2 ，基因值为 6 。1 是缺失的最小基因值。
+// - 3：子树内包含节点 [3,4,5] ，基因值分别为 [2,1,3] 。4 是缺失的最小基因值。
+// - 4：子树内只包含节点 4 ，基因值为 1 。2 是缺失的最小基因值。
+// - 5：子树内只包含节点 5 ，基因值为 3 。1 是缺失的最小基因值。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/smallest-missing-genetic-value-in-each-subtree
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 // 思路
 // 暴力解法，遍历所有节点，记录所有节点及其子树出现过的基因，对每个节点的序列排序并求最小未出现的基因，时间复杂路O(n^2*logn)，超时
 // 出现在一个节点的值，仅会出现在它及它到根节点的路径上，记录每条路径上出现过的最小数字即可
-func smallestMissingValueSubtree(parents []int, nums []int) []int {
+func smallestMissingValueSubtreeV1(parents []int, nums []int) []int {
     flag := make([][]int, len(parents))
     for i := 0; i < len(flag); i++ {
         flag[i] = make([]int, 0)
@@ -40,3 +53,14 @@ func smallestMissingValueSubtree(parents []int, nums []int) []int {
     
     return res
 }
+
+// 思路
+// 
+func smallestMissingValueSubtreeV2(parents []int, nums []int) []int {
+
+}
+
+// 输入：parents = [-1,0,1,0,3,3], nums = [5,4,6,2,1,3]
+// 输出：[7,1,1,4,2,1]
+// 找到基因中为1的节点，为找到则结果为[1]
+// 从基因为1的节点反向深度优先遍历，遍历路径上的最小基因均为1，则未出现在该路径上的节点最小路径均为1
