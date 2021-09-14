30d789a06d7d2cf611b3610083879507b244f37b
ladlod <tian_zw@qq.com>
2021-09-14 21:45:55 +0800

    add 407

--- common_list/leetcode407.go
+++ common_list/leetcode407.go
@@ -1,16 +1,24 @@
 package common_list
 
+import "fmt"
+
 // 给你一个 m x n 的矩阵，其中的值均为非负整数，代表二维高度图每个单元的高度，请计算图中形状最多能接多少体积的雨水。
 
 // 思路
 // 小顶堆记录当前最短板，
-// 遍历缩圈（怎么遍历）
-func trapRainWater(heightMap [][]int) int {
+// 堆顶出堆，入堆堆四个方向的板子，res += 堆顶高度-入堆的板子高度
+func TrapRainWater(heightMap [][]int) int {
 	if len(heightMap) <= 0 {
 		return 0
 	}
 	heap := &Heap{
 		heap: make([]*Pos, 0),
+	}
+
+	// 记录节点是否遍历过
+	flag := make([][]bool, len(heightMap))
+	for i := 0; i < len(heightMap); i++ {
+		flag[i] = make([]bool, len(heightMap[i]))
 	}
 
 	// 外圈入堆
@@ -25,6 +33,8 @@ func trapRainWater(heightMap [][]int) int {
 			x:   len(heightMap) - 1,
 			y:   i,
 		})
+		flag[0][i] = true
+		flag[len(heightMap)-1][i] = true
 	}
 	for j := 1; j < len(heightMap)-1; j++ {
 		heap.insert(&Pos{
@@ -37,12 +47,88 @@ func trapRainWater(heightMap [][]int) int {
 			x:   j,
 			y:   len(heightMap[0]) - 1,
 		})
+		flag[j][0] = true
+		flag[j][len(heightMap[0])-1] = true
 	}
 
 	ans := 0
+	fmt.Println(flag, ans)
 	for len(heap.heap) > 0 {
+		top := heap.pop()
+		if top.x > 0 && !flag[top.x-1][top.y] {
+			flag[top.x-1][top.y] = true
+			if heightMap[top.x-1][top.y] < top.val {
+				ans += top.val - heightMap[top.x-1][top.y]
+				heap.insert(&Pos{
+					val: top.val,
+					x:   top.x - 1,
+					y:   top.y,
+				})
+			} else {
+				heap.insert(&Pos{
+					val: heightMap[top.x-1][top.y],
+					x:   top.x - 1,
+					y:   top.y,
+				})
+			}
+		}
 
+		if top.y > 0 && !flag[top.x][top.y-1] {
+			flag[top.x][top.y-1] = true
+			if heightMap[top.x][top.y-1] < top.val {
+				ans += top.val - heightMap[top.x][top.y-1]
+				heap.insert(&Pos{
+					val: top.val,
+					x:   top.x,
+					y:   top.y - 1,
+				})
+			} else {
+				heap.insert(&Pos{
+					val: heightMap[top.x][top.y-1],
+					x:   top.x,
+					y:   top.y - 1,
+				})
+			}
+		}
+
+		if top.x+1 < len(heightMap) && !flag[top.x+1][top.y] {
+			flag[top.x+1][top.y] = true
+			if heightMap[top.x+1][top.y] < top.val {
+				ans += top.val - heightMap[top.x+1][top.y]
+				heap.insert(&Pos{
+					val: top.val,
+					x:   top.x + 1,
+					y:   top.y,
+				})
+			} else {
+				heap.insert(&Pos{
+					val: heightMap[top.x+1][top.y],
+					x:   top.x + 1,
+					y:   top.y,
+				})
+			}
+		}
+
+		if top.y+1 < len(heightMap[0]) && !flag[top.x][top.y+1] {
+			flag[top.x][top.y+1] = true
+			if heightMap[top.x][top.y+1] < top.val {
+				ans += top.val - heightMap[top.x][top.y+1]
+				heap.insert(&Pos{
+					val: top.val,
+					x:   top.x,
+					y:   top.y + 1,
+				})
+			} else {
+				heap.insert(&Pos{
+					val: heightMap[top.x][top.y+1],
+					x:   top.x,
+					y:   top.y + 1,
+				})
+			}
+		}
+		fmt.Println(flag, ans)
 	}
+
 	return ans
 }
 
@@ -62,13 +148,11 @@ func (h *Heap) insert(v *Pos) {
 }
 
 func (h *Heap) put(v *Pos) {
-	if v.val > h.heap[0].val {
-		h.heap[0] = v
-		h.shiftDown(0)
-	}
+	h.heap[0] = v
+	h.shiftDown(0)
 }
 
-func (h *Heap) pop(v *Pos) *Pos {
+func (h *Heap) pop() *Pos {
 	res := h.heap[0]
 	h.heap[0] = h.heap[len(h.heap)-1]
 	h.heap = h.heap[:len(h.heap)-1]
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/ladlod/leetcode/common_list"
 )
 
 func main() {
-	fmt.Println((-1) / 2)
+	fmt.Println(common_list.TrapRainWater([][]int{{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}}))
 }
